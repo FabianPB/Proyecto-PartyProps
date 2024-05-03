8be68684d864e5315959c9638243818bafcb46a6
fabian <fapalacio@unicesar.edu.co>
2024-05-03 14:09:48 -0500

    Se termino repositorio inventario

--- DAL/RepositorioInventario.cs
+++ DAL/RepositorioInventario.cs
@@ -28,5 +28,86 @@ namespace DAL
             var articulo = new Articulo(idArticulo, nombreArticulo, descripcion, precioAlquiler, existencias);
             return articulo;
         }
+        public Articulo EncontrarArticulo(string id)
+        {
+            var FileArticulo = "./Articulo.csv";
+            var opciones = new FileStreamOptions()
+            {
+                Access = FileAccess.Read
+            };
+
+            using var leer = new StreamReader(FileArticulo, opciones);
+            var linea = leer.ReadLine();
+            while (linea != null)
+            {
+                var valorLineas = linea.Split(','); // Agregamos esta línea para dividir la línea en valores
+                var idArticulo = valorLineas[0];
+                var nombreArticulo = valorLineas[1];
+                var descripcion = valorLineas[2];
+                var precioAlquiler = double.Parse(valorLineas[3]);
+                var existencias = int.Parse(valorLineas[4]);
+                var articulo = new Articulo(idArticulo, nombreArticulo, descripcion, precioAlquiler, existencias);
+
+                if (idArticulo == id)
+                {
+                    return articulo;
+                }
+                linea = leer.ReadLine();
+            }
+            return null;
+        }
+
+        public List<Articulo> EncontrarArticulos()
+        {
+            var articulos = new List<Articulo>();
+            var opciones = new FileStreamOptions()
+            {
+                Access = FileAccess.Read
+            };
+
+            using var leer = new StreamReader(FileInventario, opciones);
+            var linea = leer.ReadLine();
+            while (linea != null)
+            {
+                var valorLineas = linea.Split(',');
+                Articulo articulo = MapearInventario(valorLineas); // Corregido el nombre del método
+                articulos.Add(articulo); // Agregado el objeto articulo, no Articulo
+                linea = leer.ReadLine();
+            }
+            return articulos; // Devolver la lista de articulos
+        }
+
+        public void EliminarArticulo(Articulo ArticuloEliminar)
+        {
+            var articulos = EncontrarArticulos();
+            var ArticuloEncontrado = articulos.FirstOrDefault(articulo => articulo.idArticulo == ArticuloEliminar.idArticulo);
+            if (ArticuloEncontrado == null)
+                return;
+            articulos.Remove(ArticuloEncontrado);
+            var repositorioInventario = new RepositorioInventario();
+            foreach (var articulo in articulos)
+            {
+                repositorioInventario.Registrar(articulo);
+            }
+        }
+        public void ModificarArticulo(Articulo ArticuloModificar)
+        {
+            var articulos = EncontrarArticulos();
+            var ArticuloEncontrado = articulos.FirstOrDefault(articulo => articulo.idArticulo == ArticuloModificar.idArticulo);
+            if (ArticuloEncontrado == null)
+                return;
+
+            // Remover el artículo encontrado de la lista
+            articulos.Remove(ArticuloEncontrado);
+
+            // Agregar el artículo modificado a la lista
+            articulos.Add(ArticuloModificar);
+
+            // Guardar los cambios en el archivo
+            foreach (var articulo in articulos)
+            {
+                Registrar(articulo);
+            }
+        }
     }
 }
