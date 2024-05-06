df5bb153d6df3563e8c129c9ff0cac45cde114e2
HAROLD <harol3044@gmail.com>
2024-05-05 20:44:08 -0500

    Se cambió las listbox por datagridview

--- Projectpt1/INVENTARIO.Designer.cs
+++ Projectpt1/INVENTARIO.Designer.cs
@@ -28,125 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label2 = new System.Windows.Forms.Label();
-            this.ListaArticulos = new System.Windows.Forms.ListBox();
-            this.ListaReservas = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnActualizar = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            label2 = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            btnActualizar = new System.Windows.Forms.Button();
+            dataGridArtículos = new System.Windows.Forms.DataGridView();
+            dataGridReservas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridArtículos).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridReservas).BeginInit();
+            SuspendLayout();
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(156, 34);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(454, 32);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Inventario de Artículos y Reservas";
-            // 
-            // ListaArticulos
-            // 
-            this.ListaArticulos.FormattingEnabled = true;
-            this.ListaArticulos.Location = new System.Drawing.Point(110, 146);
-            this.ListaArticulos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.ListaArticulos.Name = "ListaArticulos";
-            this.ListaArticulos.Size = new System.Drawing.Size(228, 251);
-            this.ListaArticulos.TabIndex = 4;
-            // 
-            // ListaReservas
-            // 
-            this.ListaReservas.FormattingEnabled = true;
-            this.ListaReservas.Location = new System.Drawing.Point(417, 146);
-            this.ListaReservas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.ListaReservas.Name = "ListaReservas";
-            this.ListaReservas.Size = new System.Drawing.Size(231, 251);
-            this.ListaReservas.TabIndex = 5;
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            label2.Location = new System.Drawing.Point(208, 52);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(555, 39);
+            label2.TabIndex = 3;
+            label2.Text = "Inventario de Artículos y Reservas";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(147, 115);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(164, 23);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Lista de Artículos";
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            label1.Location = new System.Drawing.Point(196, 177);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(199, 27);
+            label1.TabIndex = 6;
+            label1.Text = "Lista de Artículos";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(458, 115);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(166, 23);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Lista de Reservas";
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            label3.Location = new System.Drawing.Point(611, 177);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(200, 27);
+            label3.TabIndex = 7;
+            label3.Text = "Lista de Reservas";
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(384, 421);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(85, 41);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Eliminar";
-            this.button1.UseVisualStyleBackColor = false;
+            button1.BackColor = System.Drawing.Color.Red;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            button1.Location = new System.Drawing.Point(512, 648);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(113, 63);
+            button1.TabIndex = 8;
+            button1.Text = "Eliminar";
+            button1.UseVisualStyleBackColor = false;
             // 
             // btnActualizar
             // 
-            this.btnActualizar.BackColor = System.Drawing.Color.Lime;
-            this.btnActualizar.FlatAppearance.BorderSize = 0;
-            this.btnActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnActualizar.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnActualizar.Location = new System.Drawing.Point(281, 421);
-            this.btnActualizar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnActualizar.Name = "btnActualizar";
-            this.btnActualizar.Size = new System.Drawing.Size(85, 41);
-            this.btnActualizar.TabIndex = 9;
-            this.btnActualizar.Text = "Actualizar";
-            this.btnActualizar.UseVisualStyleBackColor = false;
-            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            btnActualizar.BackColor = System.Drawing.Color.Lime;
+            btnActualizar.FlatAppearance.BorderSize = 0;
+            btnActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnActualizar.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            btnActualizar.Location = new System.Drawing.Point(375, 648);
+            btnActualizar.Name = "btnActualizar";
+            btnActualizar.Size = new System.Drawing.Size(113, 63);
+            btnActualizar.TabIndex = 9;
+            btnActualizar.Text = "Actualizar";
+            btnActualizar.UseVisualStyleBackColor = false;
+            btnActualizar.Click += btnActualizar_Click;
+            // 
+            // dataGridArtículos
+            // 
+            dataGridArtículos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridArtículos.Location = new System.Drawing.Point(151, 217);
+            dataGridArtículos.Name = "dataGridArtículos";
+            dataGridArtículos.RowHeadersWidth = 51;
+            dataGridArtículos.Size = new System.Drawing.Size(300, 400);
+            dataGridArtículos.TabIndex = 10;
+            // 
+            // dataGridReservas
+            // 
+            dataGridReservas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridReservas.Location = new System.Drawing.Point(563, 217);
+            dataGridReservas.Name = "dataGridReservas";
+            dataGridReservas.RowHeadersWidth = 51;
+            dataGridReservas.Size = new System.Drawing.Size(300, 400);
+            dataGridReservas.TabIndex = 11;
             // 
             // Inventario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(758, 481);
-            this.Controls.Add(this.btnActualizar);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.ListaReservas);
-            this.Controls.Add(this.ListaArticulos);
-            this.Controls.Add(this.label2);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "Inventario";
-            this.Text = "INVENTARIO";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.Silver;
+            ClientSize = new System.Drawing.Size(1011, 740);
+            Controls.Add(dataGridReservas);
+            Controls.Add(dataGridArtículos);
+            Controls.Add(btnActualizar);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            Name = "Inventario";
+            Text = "INVENTARIO";
+            ((System.ComponentModel.ISupportInitialize)dataGridArtículos).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridReservas).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox ListaArticulos;
-        private System.Windows.Forms.ListBox ListaReservas;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.DataGridView dataGridArtículos;
+        private System.Windows.Forms.DataGridView dataGridReservas;
     }
 }
